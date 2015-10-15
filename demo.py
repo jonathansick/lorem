b8a7ba06cb86b83f4cf999d7466df3cad6a5f7e0
Jonathan Sick <jonathansick@mac.com>
2015-10-15 16:06:34 -0700

    Demo file showing that long docstrings are bad.

---
+++ demo.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+Demo module demonstrating wide docstrings on GitHub.
+
+Wolf blog mixtape Vice quinoa, selvage gastropub cold-pressed. Helvetica tattooed PBR&B vinyl Neutra. Roof
+party kitsch sustainable small batch, street art biodiesel gluten-free seitan four loko master cleanse bicycle
+rights messenger bag migas. Squid 8-bit Thundercats gentrify, try-hard deep v drinking vinegar banjo
+fingerstache cronut roof party chia four dollar toast Shoreditch. Bitters lo-fi Bushwick flannel. Fashion axe
+kitsch synth single-origin coffee. Fixie cardigan narwhal Carles selvage.
+
+American Apparel scenester jean shorts, cred gentrify Helvetica aesthetic leggings sartorial you probably
+haven't heard of them tofu cardigan. Small batch Wes Anderson whatever bitters, Odd Future retro jean shorts
+tofu four loko +1 seitan freegan iPhone messenger bag viral. Tilde VHS dreamcatcher fingerstache flannel
+bicycle rights. Bicycle rights health goth freegan mixtape, small batch plaid scenester keytar Vice
+Intelligentsia shabby chic sustainable Truffaut Schlitz Williamsburg. Hoodie PBR chia Pinterest. Craft beer
+Thundercats readymade fingerstache. Butcher bitters ugh hoodie, American Apparel vegan crucifix gentrify kale
+chips cray ennui Brooklyn bespoke.
+
+Stumptown Brooklyn brunch direct trade leggings dreamcatcher, put a bird on it craft beer actually DIY XOXO
+pork belly. Pitchfork Schlitz stumptown whatever, Godard Truffaut normcore. McSweeney's master cleanse vinyl
+lomo XOXO cray retro. Wayfarers mixtape retro fixie tousled, next level cred Truffaut butcher organic squid.
+Keytar Wes Anderson health goth Blue Bottle, Pinterest typewriter meggings dreamcatcher kogi PBR&B cornhole.
+Echo Park hashtag migas 3 wolf moon, before they sold out health goth meditation tofu leggings Kickstarter
+synth gastropub Marfa chillwave food truck. Sartorial leggings Blue Bottle Etsy authentic street art.
+
+VHS stumptown DIY occupy actually swag, Thundercats craft beer bitters retro kitsch crucifix bicycle rights
+Pinterest. Keytar meditation farm-to-table put a bird on it art party food truck. Dreamcatcher pour-over trust
+fund selvage, lo-fi skateboard bicycle rights direct trade mustache beard. Occupy lomo cold-pressed, tattooed
+gluten-free try-hard locavore readymade next level pour-over Bushwick art party leggings. Austin messenger bag
+before they sold out taxidermy, Odd Future organic pug bitters cold-pressed retro. Seitan keytar Pinterest
+jean shorts fingerstache, health goth Carles pour-over leggings ugh Schlitz try-hard authentic. Letterpress
+mlkshk beard, farm-to-table health goth church-key forage four loko plaid.  """
+
+
+def main():
+    """Do nothing.
+
+    Whatever listicle kogi, literally dreamcatcher scenester tote bag Pitchfork Neutra readymade DIY. Salvia
+    messenger bag blog scenester heirloom migas. Marfa tousled umami, mumblecore tattooed hoodie jean shorts
+    plaid whatever. Messenger bag Truffaut roof party, chillwave Bushwick seitan 90's leggings Brooklyn
+    gluten-free blog mixtape Pitchfork four loko Thundercats. Shoreditch listicle pickled, Thundercats Austin
+    keffiyeh four dollar toast selvage flannel chambray umami butcher drinking vinegar. Cornhole biodiesel fap
+    drinking vinegar selvage, salvia actually pour-over next level cardigan. 90's letterpress polaroid cred
+    typewriter iPhone Portland, migas craft beer pug disrupt Truffaut Banksy health goth.
+
+    Polaroid iPhone distillery synth banjo. Narwhal bitters viral Pinterest gastropub retro, artisan
+    meditation kitsch. Banksy tousled health goth, pour-over cred keffiyeh freegan put a bird on it Blue
+    Bottle skateboard. Organic cronut fap, semiotics sustainable Marfa polaroid paleo Odd Future Echo Park
+    High Life literally. Banjo meggings iPhone 3 wolf moon. Lumbersexual banh mi stumptown meggings, street
+    art keffiyeh scenester Godard vegan small batch Marfa. Austin four loko Portland Carles distillery PBR meh
+    selvage, church-key art party umami small batch try-hard synth Vice.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
